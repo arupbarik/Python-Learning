a33c35e67b3031b35008d4202ced260ef22b4ce4
Arup Barik <arupbarik323@gmail.com>
2026-04-17 01:41:09 +0530

    just learn variable and their types in python

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
 print ("Hello World")
+x=5
+print(x, type(x))
+fruits=["apple","banana","cherry"]
+print("fruits are:", fruits, type(fruits))
+y= float(24)
+print(y, type(y))
+z= str(24)
+print(z, type(z))
