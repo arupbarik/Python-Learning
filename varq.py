4300cdfd2e2f86a288df614c571d674a6f1772e1
Arup Barik <arupbarik323@gmail.com>
2026-05-28 12:25:28 +0530

    variable ques done

---
+++ varq.py
@@ -0,0 +1,20 @@
+print("questions for variable topic")
+# Assign x = 10 and y = 3.14. Print their types using type().
+x=10
+y=3.14
+print(type(x))
+print(type(y))
+
+# Convert the string "42" to an integer and "3.14" to a float.
+a="42"
+b="3.14"
+a=int(a)
+b=float(b)
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+
+# What does id(x) return? Demonstrate how Python handles variable assignment using references.
+# Explain dynamic typing with an example.
+# please explain this above ques
